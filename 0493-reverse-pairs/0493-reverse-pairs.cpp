eeea22d8e24cf3e82fc6951540a0fb3e61cbba8e
Subham Prasad <ansuprasad553@gmail.com>
2026-08-21 12:58:16 +0530

    Update 0493-reverse-pairs.cpp

--- 0493-reverse-pairs/0493-reverse-pairs.cpp
+++ 0493-reverse-pairs/0493-reverse-pairs.cpp
@@ -1,15 +1,80 @@
 class Solution {
 public:
-    int reversePairs(vector<int>& nums) {
-        int cnt=0;
-        int n=nums.size();
-        for (int i=0;i<n-1;i++){
-            for (int j=i+1;j<n;j++){
-                if(nums[i]>nums[j]*2){
-                    cnt++;
-                }
+
+    int merge(vector<int>& nums, int low, int mid, int high) {
+
+        int cnt = 0;
+
+        // Count reverse pairs
+        int j = mid + 1;
+
+        for (int i = low; i <= mid; i++) {
+
+            while (j <= high &&
+                   (long long)nums[i] > 2LL * nums[j]) {
+                j++;
+            }
+
+            cnt += j - (mid + 1);
+        }
+
+        // Normal merge
+        vector<int> temp;
+
+        int i = low;
+        j = mid + 1;
+
+        while (i <= mid && j <= high) {
+
+            if (nums[i] <= nums[j]) {
+                temp.push_back(nums[i]);
+                i++;
+            }
+            else {
+                temp.push_back(nums[j]);
+                j++;
             }
         }
+
+        while (i <= mid) {
+            temp.push_back(nums[i]);
+            i++;
+        }
+
+        while (j <= high) {
+            temp.push_back(nums[j]);
+            j++;
+        }
+
+        for (int k = low; k <= high; k++) {
+            nums[k] = temp[k - low];
+        }
+
         return cnt;
+    }
+
+
+    int mergeSort(vector<int>& nums, int low, int high) {
+
+        if (low >= high)
+            return 0;
+
+        int mid = low + (high - low) / 2;
+
+        int cnt = 0;
+
+        cnt += mergeSort(nums, low, mid);
+
+        cnt += mergeSort(nums, mid + 1, high);
+
+        cnt += merge(nums, low, mid, high);
+
+        return cnt;
+    }
+
+
+    int reversePairs(vector<int>& nums) {
+
+        return mergeSort(nums, 0, nums.size() - 1);
     }
 };
