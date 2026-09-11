8ad66a1bdf406d8d1cc1208f475c061e101aaa11
Subham Prasad <ansuprasad553@gmail.com>
2026-09-11 19:24:05 +0530

    Time: 0 ms (100%), Space: 28.9 MB (84.69%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
@@ -1,31 +1,39 @@
 class Solution {
 public:
     int totalNumbers(vector<int>& digits) {
-        unordered_set<int> st;
-        int n = digits.size();
+        int freq[10] = {};
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                for (int k = 0; k < n; k++) {
+        for (int x : digits)
+            freq[x]++;
 
-                    if (i == j || j == k || i == k)
-                        continue;
+        int ans = 0;
 
-                    if (digits[i] == 0)
-                        continue;
+        for (int h = 1; h <= 9; h++) {
 
-                    if (digits[k] % 2 != 0)
-                        continue;
+            if (freq[h] == 0)
+                continue;
 
-                    int num = digits[i] * 100 +
-                              digits[j] * 10 +
-                              digits[k];
+            freq[h]--;
 
-                    st.insert(num);
+            for (int t = 0; t <= 9; t++) {
+
+                if (freq[t] == 0)
+                    continue;
+
+                freq[t]--;
+
+                for (int u = 0; u <= 8; u += 2) {
+
+                    if (freq[u] > 0)
+                        ans++;
                 }
+
+                freq[t]++;
             }
+
+            freq[h]++;
         }
 
-        return st.size();
+        return ans;
     }
 };
