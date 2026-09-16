9ef8c05e5d3848c13aeb935c4503e144c7fb2643
Subham Prasad <ansuprasad553@gmail.com>
2026-09-16 20:11:08 +0530

    Time: 3 ms (5.66%), Space: 11.5 MB (60.61%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for (int i=digits.size()-1;i>=0;i--){
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
+            }
+            else if(digits[i]==9){
+                digits[i]=0;
+            }
+        }
+        digits.insert(digits.begin(),1);   //when the value is 999..so it turns to 1000
+        return digits;
+    }
+};
